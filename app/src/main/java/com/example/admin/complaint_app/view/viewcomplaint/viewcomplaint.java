f5acd1e0f2f61c67b47e5c70d0efaf0caf3bd276
patilmanali410 <48617565+patilmanali410@users.noreply.github.com>
2019-03-18 22:45:21 +0530

    added all java files succesfully

---
+++ app/src/main/java/com/example/admin/complaint_app/view/viewcomplaint/viewcomplaint.java
@@ -0,0 +1,13 @@
+package com.example.admin.complaint_app.view.viewcomplaint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class viewcomplaint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewcomplaint);
+    }
+}
