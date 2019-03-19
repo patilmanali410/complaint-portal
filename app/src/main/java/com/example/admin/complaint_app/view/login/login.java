807a94fd94e8a9db53f13e1497850b3bc4fa6151
patilmanali410 <48617565+patilmanali410@users.noreply.github.com>
2019-03-19 23:18:28 +0530

    done with profile page

--- app/src/main/java/com/example/admin/complaint_app/view/login/login.java
+++ app/src/main/java/com/example/admin/complaint_app/view/login/login.java
@@ -1,15 +1,33 @@
 package com.example.admin.complaint_app.view.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.admin.complaint_app.R;
+import com.example.admin.complaint_app.view.profile.profile;
 
 public class login extends AppCompatActivity {
-
+    private Button login_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login_button =(Button)findViewById(R.id.dologin);
+
+        login_button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                openProfilepage();
+            }
+        });
+
+    }
+
+    public void openProfilepage(){
+        Intent intent=new Intent(this,profile.class);
+        startActivity(intent);
     }
 }
