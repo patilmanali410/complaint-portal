1ee29146b5b39c7cd46916c268290f3139283d6b
patilmanali410 <48617565+patilmanali410@users.noreply.github.com>
2019-03-22 23:54:07 +0530

    recyclerview added

--- app/src/main/java/com/example/admin/complaint_app/view/profile/profile.java
+++ app/src/main/java/com/example/admin/complaint_app/view/profile/profile.java
@@ -12,9 +12,18 @@ import com.example.admin.complaint_app.view.profile.HomeFragment;
 import com.example.admin.complaint_app.view.profile.DashboardFragment;
 import com.example.admin.complaint_app.view.profile.NotificationsFragment;
 import com.example.admin.complaint_app.view.profile.ProfileFragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.LinearLayoutManager;
+import java.util.List;
+import java.util.ArrayList;
 
 //implement the interface OnNavigationItemSelectedListener in your activity class
 public class profile extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    //a list to store all the products
+    List<Product> productList;
+
+    //the recyclerview
+    RecyclerView recyclerView;
 
 
     @Override
@@ -28,6 +37,51 @@ public class profile extends AppCompatActivity implements BottomNavigationView.O
         //getting bottom navigation view and attaching the listener
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //initializing the productlist
+        productList = new ArrayList<>();
+
+
+        //adding some items to our list
+        productList.add(
+                new Product(
+                        1,
+                        "Apple MacBook Air Core i5 5th Gen - (8 GB/128 GB SSD/Mac OS Sierra)",
+                        "13.3 inch, Silver, 1.35 kg",
+                        4.3,
+                        60000,
+                        R.drawable.ic_home_black_24dp));
+
+        productList.add(
+                new Product(
+                        1,
+                        "Dell Inspiron 7000 Core i5 7th Gen - (8 GB/1 TB HDD/Windows 10 Home)",
+                        "14 inch, Gray, 1.659 kg",
+                        4.3,
+                        60000,
+                        R.drawable.ic_home_black_24dp));
+
+        productList.add(
+                new Product(
+                        1,
+                        "Microsoft Surface Pro 4 Core m3 6th Gen - (4 GB/128 GB SSD/Windows 10)",
+                        "13.3 inch, Silver, 1.35 kg",
+                        4.3,
+                        60000,
+                        R.drawable.ic_home_black_24dp));
+
+        //creating recyclerview adapter
+        ProductAdapter adapter = new ProductAdapter(this, productList);
+
+        //setting adapter to recyclerview
+        recyclerView.setAdapter(adapter);
+
+
+
     }
 
 
@@ -67,4 +121,7 @@ public class profile extends AppCompatActivity implements BottomNavigationView.O
         }
         return false;
     }
+
+
+
 }
