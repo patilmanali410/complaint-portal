4bf7f5c39a5dfe7ce368cad2a3c3ee407f994083
jainritik153 <jainritik153@gmail.com>
2019-04-03 19:26:18 +0530

    added sign up functionality

--- app/src/main/java/com/example/admin/complaint_app/models/Student.java
+++ app/src/main/java/com/example/admin/complaint_app/models/Student.java
@@ -1,54 +1,21 @@
 package com.example.admin.complaint_app.models;
 
 public class Student {
+  public String name;
+  public String email;
 
-    public Number student_id;
 
-    public String studentName;
+  public Student(String name, String email) {
+  this.name = name;
+  this.email = email;
+ }
 
-    public String studentDepartment;
+ public String getName() {
+  return name;
+ }
 
-    public String studentYear;
+ public String getEmail() {
+  return email;
+ }
 
-    public Number studentContact;
-
-    public void setStudent_id(Number student_id) {
-        this.student_id = student_id;
-    }
-
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
-    }
-
-    public void setStudentDepartment(String studentDepartment) {
-        this.studentDepartment = studentDepartment;
-    }
-
-    public void setStudentYear(String studentYear) {
-        this.studentYear = studentYear;
-    }
-
-    public void setStudentContact(Number studentContact) {
-        this.studentContact = studentContact;
-    }
-
-    public Number getStudent_id() {
-        return student_id;
-    }
-
-    public String getStudentName() {
-        return studentName;
-    }
-
-    public String getStudentDepartment() {
-        return studentDepartment;
-    }
-
-    public String getStudentYear() {
-        return studentYear;
-    }
-
-    public Number getStudentContact() {
-        return studentContact;
-    }
 }
